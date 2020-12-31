ef915f7e1fb55694128d61526dd35c384efbe847
ZhouBing <984525713@qq.com>
2020-12-31 14:25:39 +0800

    update eval_hook and checkpoint_hook

---
+++ mmpose/core/checkpoint/checkpoint_hook.py
@@ -0,0 +1,108 @@
+import os
+from mmcv.runner.dist_utils import allreduce_params, master_only
+from mmcv.runner.hooks import HOOKS
+from mmcv.runner.checkpoint import save_checkpoint
+from mmcv.runner.hooks.checkpoint import CheckpointHook
+
+@HOOKS.register_module()
+class MyCheckpointHook(CheckpointHook):
+    """Save checkpoints periodically.
+
+    Args:
+        interval (int): The saving period. If ``by_epoch=True``, interval
+            indicates epochs, otherwise it indicates iterations.
+            Default: -1, which means "never".
+        by_epoch (bool): Saving checkpoints by epoch or by iteration.
+            Default: True.
+        save_optimizer (bool): Whether to save optimizer state_dict in the
+            checkpoint. It is usually used for resuming experiments.
+            Default: True.
+        out_dir (str, optional): The directory to save checkpoints. If not
+            specified, ``runner.work_dir`` will be used by default.
+        max_keep_ckpts (int, optional): The maximum checkpoints to keep.
+            In some cases we want only the latest few checkpoints and would
+            like to delete old ones to save the disk space.
+            Default: -1, which means unlimited.
+        sync_buffer (bool): Whether to synchronize buffers in different
+            gpus. Default: False.
+        start_epoch (int): Begin saving checkpoint from the given epoch.
+        with_indicator: Whether mark checkpoint file with epoch or iteration.
+            If set False, only the latest checkpoint will be saved.  
+            Default: True
+    """
+    def __init__(self,
+                 interval=-1,
+                 by_epoch=True,
+                 save_optimizer=True,
+                 out_dir=None,
+                 max_keep_ckpts=-1,
+                 sync_buffer=False,
+                 **kwargs):
+        self.interval = interval
+        self.by_epoch = by_epoch
+        self.save_optimizer = save_optimizer
+        self.out_dir = out_dir
+        self.max_keep_ckpts = max_keep_ckpts
+        self.args = kwargs
+        self.sync_buffer = sync_buffer
+        self.with_indicator = None
+        self.start_epoch = -1
+
+    def after_train_epoch(self, runner):
+        if not self.by_epoch or not self.every_n_epochs(runner, self.interval):
+            return
+
+        self.start_epoch = self.args.pop('start_epoch', -1) if self.start_epoch == -1 else self.start_epoch
+        if self.start_epoch != -1 and (runner.epoch + 1) < self.start_epoch:
+            return
+
+        runner.logger.info(f'Saving checkpoint at {runner.epoch + 1} epochs')
+        if self.sync_buffer:
+            allreduce_params(runner.model.buffers())
+        self._save_checkpoint(runner)
+
+    @master_only
+    def _save_checkpoint(self, runner):
+        """Save the current checkpoint and delete unwanted checkpoint."""
+        if not self.out_dir:
+            self.out_dir = runner.work_dir
+        self.with_indicator = self.args.pop('with_indicator', True) if \
+            self.with_indicator else self.with_indicator
+        if self.with_indicator:
+            runner.save_checkpoint(
+                self.out_dir, save_optimizer=self.save_optimizer, **self.args)
+        else:
+            if self.by_epoch:
+                save_checkpoint(runner.model, os.path.join(self.out_dir, 'epoch_latest.pth'))
+            else:
+                save_checkpoint(runner.model, os.path.join(self.out_dir, 'iter_latest.pth'))
+
+        if runner.meta is not None:
+            if self.by_epoch:
+                cur_ckpt_filename = self.args.get(
+                    'filename_tmpl', 'epoch_{}.pth').format(runner.epoch + 1)
+            else:
+                cur_ckpt_filename = self.args.get(
+                    'filename_tmpl', 'iter_{}.pth').format(runner.iter + 1)
+            runner.meta.setdefault('hook_msgs', dict())
+            runner.meta['hook_msgs']['last_ckpt'] = os.path.join(
+                self.out_dir, cur_ckpt_filename)
+        # remove other checkpoints
+        if self.max_keep_ckpts > 0:
+            if self.by_epoch:
+                name = 'epoch_{}.pth'
+                current_ckpt = runner.epoch + 1
+            else:
+                name = 'iter_{}.pth'
+                current_ckpt = runner.iter + 1
+            redundant_ckpts = range(
+                current_ckpt - self.max_keep_ckpts * self.interval, 0,
+                -self.interval)
+            filename_tmpl = self.args.get('filename_tmpl', name)
+            for _step in redundant_ckpts:
+                ckpt_path = os.path.join(self.out_dir,
+                                         filename_tmpl.format(_step))
+                if os.path.exists(ckpt_path):
+                    os.remove(ckpt_path)
+                else:
+                    break
